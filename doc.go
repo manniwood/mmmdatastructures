6c7742c664f6f75db673862951fffe6da20f78ad
Manni Wood <manni.lee.wood@gmail.com>
2017-10-25 14:52:43 -0400

    Adds top-level doc.go

---
+++ doc.go
@@ -0,0 +1,4 @@
+// Package mmmdatastructures implements common datastructures
+// that are hard-coded to particular Go types rather than
+// using the empty interface and casting.
+package mmmdatastructures
