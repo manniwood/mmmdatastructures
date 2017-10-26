b8efe07721d78cdcc065d556e816a9188411d25a
Manni Wood <manni.lee.wood@gmail.com>
2017-10-26 18:22:57 -0400

    Adds int maxheap

--- doc.go
+++ doc.go
@@ -1,4 +1,14 @@
-// Package mmmdatastructures implements common datastructures
+// Package mmmdatastructures implements common data structures
 // that are hard-coded to particular Go types rather than
 // using the empty interface and casting.
+//
+// This means that there is a lot of repetition between
+// implementations; no effort has been made to DRY up the code.
+//
+// On the other hand, if you need precisely one of these
+// data structrues, there's no extraneous fluff/indirection/abstraction.
+//
+// Also, because of the simplistic nature of these data structures,
+// they may be good starting places for copying and customizing
+// for your own projects.
 package mmmdatastructures
---
+++ ints/maxheap/maxheap.go
@@ -0,0 +1,138 @@
+// Package maxheap implements a binary max heap for ints.
+//
+// It is an express design decision to hard-code
+// this max heap just for the int type rather than for
+// the empty interface.
+//
+package maxheap
+
+import "github.com/pkg/errors"
+
+// MaxHeap holds the data and state of the max heap.
+type MaxHeap struct {
+	data     []int
+	capacity int
+	size     int
+}
+
+// DefaultCapacity is the default capacity of the max heap
+// when constructed using New() instead of NewWithCapacity().
+const DefaultCapacity = 32
+
+// New returns a new empty max heap for ints of the default capacity.
+func New() (h *MaxHeap) {
+	return NewWithCapacity(DefaultCapacity)
+}
+
+// NewWithCapacity returns a new empty max heap for ints with the requested capacity.
+func NewWithCapacity(capacity int) (h *MaxHeap) {
+	// TODO: balk if this is not a power of two; no, just round to next nearest power of two
+	h = new(MaxHeap)
+	h.data = make([]int, capacity, capacity)
+	h.capacity = capacity
+	h.size = 0
+	return h
+}
+
+// Insert inserts an int onto the max heap. Returns an error if the size
+// of the max heap cannot be grown any more to accommodate
+// the added int.
+func (h *MaxHeap) Insert(i int) error {
+	if h.size+1 > h.capacity {
+		newCapacity := h.capacity * 2
+		// if newCapacity became negative, we have exceeded
+		// our capacity by doing one bit-shift too far
+		if newCapacity < 0 {
+			return errors.New("Capacity exceeded")
+		}
+		// NOTE: Purposefully not concerning ourselves
+		// with the error returned from Resize here, because
+		// we know our newCapacity is larger than q.capacity.
+		h.resize(newCapacity)
+	}
+	// Increase the size of the max heap. Usefully, the size
+	// is also the new last index into the backing slice.
+	// Put our new value there. Then, bubble the new value
+	// up, swapping it with its parent, until it is in the
+	// correct position in the max heap.
+	h.size++
+	h.data[h.size] = i
+	child := h.size
+	for parent := child / 2; parent > 0; parent = child / 2 {
+		if h.data[child] > h.data[parent] {
+			h.data[child], h.data[parent] = h.data[parent], h.data[child]
+		}
+		child = parent
+	}
+	return nil
+}
+
+// Size returns the current size
+// of the max heap. It also tells you how many
+// slots are being used in the slice that
+// backs the max heap.
+func (h *MaxHeap) Size() int {
+	return h.size
+}
+
+// Capacity returns the current capacity
+// of the slice that backs the queue.
+func (h *MaxHeap) Capacity() int {
+	return h.capacity
+}
+
+// resize resizes the underlying slice that backs
+// the max heap. It is made private, because we
+// want to enforce resize only being called with
+// a capacity that is twice the size of the previous
+// capacity.
+func (h *MaxHeap) resize(newCapacity int) error {
+	if newCapacity <= h.capacity {
+		return errors.Errorf("New capacity %d is not larger than current capacity %d", newCapacity, h.capacity)
+	}
+	newData := make([]int, newCapacity, newCapacity)
+	for i := 0; i < len(h.data); i++ {
+		newData[i] = h.data[i]
+	}
+	h.capacity = newCapacity
+	h.data = newData
+	return nil
+}
+
+// Peek returns the largest value from the top of the
+// heap, without removing it.
+func (h *MaxHeap) Peek() (int, error) {
+	if h.size == 0 {
+		return 0, errors.New("Heap empty")
+	}
+	return h.data[1], nil
+}
+
+// Delete returns the largest value from the top of the
+// heap, deleting it.
+func (h *MaxHeap) Delete() (int, error) {
+	if h.size == 0 {
+		return 0, errors.New("Heap empty")
+	}
+	max := h.data[1]
+	h.data[1] = h.data[h.size]
+	h.size--
+
+	parent := 1
+	for parent*2 <= h.size {
+		// make child the index of the larger of the parent's two children
+		child := parent * 2
+		if h.data[child+1] > h.data[child] {
+			child++
+		}
+		// swap the child with the parent if the child is larger
+		if h.data[parent] < h.data[child] {
+			h.data[child], h.data[parent] = h.data[parent], h.data[child]
+		} else {
+			break
+		}
+		parent = child
+	}
+
+	return max, nil
+}
