b5e0df2b4f558fcc3086076c787fdd6deeb72a5a
Manni Wood <manni.lee.wood@gmail.com>
2021-12-23 17:27:58 -0500

    Adds generic queue

---
+++ v2/queue/queue.go
@@ -0,0 +1,176 @@
+// Package queue implements a queue.
+//
+// The internal representation is a slice of T
+// that gets used as a circular buffer.
+// This is instead of a more traditional approach
+// that would use a linked list of nodes.
+// The assumption is that contiguous slabs of RAM
+// will generally provide more performance over pointers
+// to nodes potentially scattered about the heap.
+//
+// There is a downside: whereas enqueueing to a
+// linked list is always O(1), enqueueing here will
+// be O(1) except for when the internal slice of ints
+// has to be resized; then, enqueueing will be O(n)
+// where n is the size of the queue before being resized.
+//
+// Therefore, when asking for a new instance of the
+// queue, use NewWithCapacity() to pick a capacity that you
+// think won't need to grow.
+//
+// When the queue does need to grow, it always uses a capacity
+// that is twice the current capacity. Enqueue() will do this
+// doubling for you automatically.
+//
+// However, if you would like to grow the backing slice
+// yourself, to have control over 1) when the size is increased,
+// and 2) how much larger the backing slice grows, you can use
+// Resize() directly. If your code needs to ask the current
+// capacity and length of the queue, Capacity() and Length()
+// will provide those numbers.
+package queue
+
+import (
+	"constraints"
+	"errors"
+	"fmt"
+)
+
+// DefaultCapacity is the default capacity of the queue
+// when constructed using New() instead of NewWithCapacity().
+const DefaultCapacity = 32
+
+var QueueCapacityExceeded = errors.New("Queue Capacity Exceeded")
+var QueueEmpty = errors.New("Queue Empty")
+
+// Queue holds the data and state of the queue.
+type Queue[T constraints.Ordered] struct {
+	data     []T
+	head     int
+	tail     int
+	capacity int
+	length   int
+}
+
+// New returns a new empty queue for ints of the default capacity.
+func New[T constraints.Ordered]() (q *Queue[T]) {
+	return NewWithCapacity[T](DefaultCapacity)
+}
+
+// NewWithCapacity returns a new empty queue for ints with the requested capacity.
+func NewWithCapacity[T constraints.Ordered](capacity int) (q *Queue[T]) {
+	// TODO: use &{} syntax instead
+	q = new(Queue[T])
+	q.data = make([]T, capacity, capacity)
+	q.head = -1
+	q.tail = -1
+	q.capacity = capacity
+	q.length = 0
+	return q
+}
+
+// Enqueue enqueues an int. Returns an error if the size
+// of the queue cannot be grown any more to accommodate
+// the added int.
+func (q *Queue[T]) Enqueue(i T) error {
+	if q.length+1 > q.capacity {
+		newCapacity := q.capacity * 2
+		// if newCapacity became negative, we have exceeded
+		// our capacity by doing one bit-shift too far
+		if newCapacity < 0 {
+			return QueueCapacityExceeded
+		}
+		// NOTE: Purposefully not concerning ourselves
+		// with the error returned from Resize here, because
+		// we know our newCapacity is larger than q.capacity.
+		q.Resize(newCapacity)
+	}
+	q.length++
+	q.head++
+	if q.head == q.capacity {
+		q.head = 0
+	}
+	q.data[q.head] = i
+	return nil
+}
+
+// EnqueueSlice enqueues a slice of ints. Returns an error
+// if the size of the queue cannot be grown any more to accommodate
+// the added ints.
+func (q *Queue[T]) EnqueueSlice(sl []T) error {
+	for _, i := range sl {
+		err := q.Enqueue(i)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// Length returns the current length
+// of the queue. This is the same as the number of
+// slots used in the slice that
+// backs the queue.
+func (q *Queue[T]) Length() int {
+	return q.length
+}
+
+// Empty returns true if the queue is empty,
+// false otherwise.
+func (q *Queue[T]) Empty() bool {
+	return q.length == 0
+}
+
+// Capacity returns the current capacity
+// of the slice that backs the queue.
+func (q *Queue[T]) Capacity() int {
+	return q.capacity
+}
+
+// Resize resizes the underlying slice that backs
+// the queue. The Enqueue method calls this automatically
+// when the backing slice is full, but feel free to use
+// this method preemptively if your calling code has a
+// good time to do this resizing. Also, the Enqueue method
+// uses a new backing slice that is twice the size of the
+// old one; but if you call Resize yourself, you can pick
+// whatever new size you want.
+func (q *Queue[T]) Resize(newCapacity int) error {
+	if newCapacity <= q.capacity {
+		return fmt.Errorf("New capacity %d is not larger than current capacity %d", newCapacity, q.capacity)
+	}
+	newData := make([]T, newCapacity, newCapacity)
+	var err error
+	var i T
+	// Because we are using the slice as a ring buffer,
+	// head can be earlier in array than tail, so
+	// it would be strange to just copy the old (possibly
+	// partially wrapped) slice into the new slice.
+	// Instead, we may as well copy the queue in order
+	// into the new slice. The Dequeue() method gives us
+	// every element in the correct order already, so we
+	// just leverage that.
+	for err = nil; err == nil; i, err = q.Dequeue() {
+		newData = append(newData, i)
+	}
+	q.head = q.length - 1
+	q.tail = 0
+	q.capacity = newCapacity
+	q.data = newData
+	return nil
+}
+
+// Dequeue dequeues an int. It returns the dequeued int
+// or an error if the queue is empty.
+func (q *Queue[T]) Dequeue() (T, error) {
+	if q.length-1 < 0 {
+		var zero T
+		return zero, QueueEmpty
+	}
+	q.length--
+	q.tail++
+	if q.tail == q.capacity {
+		q.tail = 0
+	}
+	return q.data[q.tail], nil
+}
---
+++ v2/queue/queue_test.go
@@ -0,0 +1,170 @@
+package queue
+
+import (
+	"fmt"
+	"strconv"
+	"testing"
+)
+
+func TestCreateInt(t *testing.T) {
+	q := New[int]()
+	if q.head != -1 {
+		t.Error("Expected Head to be -1, got ", q.head)
+	}
+	if q.tail != -1 {
+		t.Error("Expected Tail to be -1, got ", q.tail)
+	}
+}
+
+func TestEnqueueInt(t *testing.T) {
+	q := New[int]()
+	q.Enqueue(5)
+	if q.tail != -1 {
+		t.Error("Expected Tail to be -1, got ", q.tail)
+	}
+	if q.head != 0 {
+		t.Error("Expected Tail to be 0, got ", q.tail)
+	}
+}
+
+func TestFillInt(t *testing.T) {
+	q := New[int]()
+	for i := 1; i <= 32; i++ {
+		q.Enqueue(i)
+	}
+	q.Enqueue(33)
+	if q.capacity != 64 {
+		t.Error("Expected capacity to double")
+	}
+}
+
+func TestDrainInt(t *testing.T) {
+	q := New[int]()
+	for i := 1; i <= 32; i++ {
+		q.Enqueue(i)
+	}
+	var i int
+	var err error
+	for j := 0; j < 32; j++ {
+		i, err = q.Dequeue()
+		if i != j+1 {
+			t.Error("Expected i to be ", j, ", got ", i)
+		}
+	}
+	if q.Length() != 0 {
+		t.Error("Expected queue length to be 0")
+	}
+	if !q.Empty() {
+		t.Error("Expected queue to be empty")
+	}
+	i, err = q.Dequeue()
+	if err == nil {
+		t.Error("Expected err to be present")
+	}
+	for j := 1; j < 35; j++ {
+		q.Enqueue(j)
+		i, err = q.Dequeue()
+		if i != j {
+			t.Error("Expected i to be ", j, ", got ", i)
+		}
+	}
+}
+
+func TestEnqueueSliceInt(t *testing.T) {
+	q := New[int]()
+	q.EnqueueSlice([]int{1, 2, 3})
+	if q.Length() != 3 {
+		t.Error("Expected queue length to be 3")
+	}
+	for j := 1; j <= 3; j++ {
+		i, err := q.Dequeue()
+		if err != nil {
+			t.Errorf("Unexpected error: %v", err)
+		}
+		if i != j {
+			t.Errorf("Expected i to be >>%v<<, got >>%v<<", j, i)
+		}
+	}
+}
+
+func TestCreateString(t *testing.T) {
+	q := New[string]()
+	if q.head != -1 {
+		t.Error("Expected Head to be -1, got ", q.head)
+	}
+	if q.tail != -1 {
+		t.Error("Expected Tail to be -1, got ", q.tail)
+	}
+}
+
+func TestEnqueueString(t *testing.T) {
+	q := New[string]()
+	q.Enqueue("5")
+	if q.tail != -1 {
+		t.Error("Expected Tail to be -1, got ", q.tail)
+	}
+	if q.head != 0 {
+		t.Error("Expected Tail to be 0, got ", q.tail)
+	}
+}
+
+func TestFillString(t *testing.T) {
+	q := New[string]()
+	for i := 1; i <= 32; i++ {
+		q.Enqueue(fmt.Sprint(i))
+	}
+	q.Enqueue("33")
+	if q.capacity != 64 {
+		t.Error("Expected capacity to double")
+	}
+}
+
+func TestDrainString(t *testing.T) {
+	q := New[string]()
+	for i := 1; i <= 32; i++ {
+		q.Enqueue(fmt.Sprint(i))
+	}
+	var i string
+	var err error
+	for j := 0; j < 32; j++ {
+		i, err = q.Dequeue()
+		if i != fmt.Sprint(j+1) {
+			t.Error("Expected i to be ", j, ", got ", i)
+		}
+	}
+	if q.Length() != 0 {
+		t.Error("Expected queue length to be 0")
+	}
+	if !q.Empty() {
+		t.Error("Expected queue to be empty")
+	}
+	i, err = q.Dequeue()
+	if err == nil {
+		t.Error("Expected err to be present")
+	}
+	for j := 1; j < 35; j++ {
+		q.Enqueue(fmt.Sprint(j))
+		i, err = q.Dequeue()
+		if i != fmt.Sprint(j) {
+			t.Error("Expected i to be ", j, ", got ", i)
+		}
+	}
+}
+
+func TestEnqueueSliceString(t *testing.T) {
+	q := New[string]()
+	q.EnqueueSlice([]string{"1", "2", "3"})
+	if q.Length() != 3 {
+		t.Error("Expected queue length to be 3")
+	}
+	for j := 1; j <= 3; j++ {
+		i, err := q.Dequeue()
+		if err != nil {
+			t.Errorf("Unexpected error: %v", err)
+		}
+		expected := strconv.Itoa(j)
+		if i != expected {
+			t.Errorf("Expected i to be >>%v<<, got >>%v<<", expected, i)
+		}
+	}
+}
